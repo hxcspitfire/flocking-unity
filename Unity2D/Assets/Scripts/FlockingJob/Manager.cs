2b2d45ecaa944e0cc7c209ff31d6d893e43e82bb
Shamim Akhtar <shamim.akhtar@gmail.com>
2021-09-15 18:54:46 +0800

    added random movement as job

--- Unity2D/Assets/Scripts/FlockingJob/Manager.cs
+++ Unity2D/Assets/Scripts/FlockingJob/Manager.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Collections;
+using Unity.Collections.LowLevel.Unsafe;
 using Unity.Jobs;
 using UnityEngine;
 using UnityEngine.EventSystems;
@@ -17,35 +19,52 @@ public class Manager : Patterns.Singleton<Manager>
   public float MinY = -100.0f;
   public float MaxY = 100.0f;
   public UI_Job UiJob;
+
+  public bool useRandomRule = true;
+  public float TickDurationRandom = 1.0f;
   #endregion
 
   TransformAccessArray transforms;
   MovementJob movementJob;
   JobHandle movementJobHandle;
+  RandomMovementJob randomMovementJob;
+  JobHandle randomMovementJobHandle;
+  [NativeDisableContainerSafetyRestriction]
+  public NativeArray<Vector3> targetDirections;
+
+  System.Random mSeed;
 
   private void OnDisable()
   {
     movementJobHandle.Complete();
+    randomMovementJobHandle.Complete();
     transforms.Dispose();
+    targetDirections.Dispose();
   }
 
   private void Start()
   {
+    mSeed = new System.Random();
     transforms = new TransformAccessArray(0, -1);
     AddBoids(BoidCount);
+
+    StartCoroutine(Coroutine_Random());
   }
 
   void AddBoids(int count)
   {
     movementJobHandle.Complete();
+    randomMovementJobHandle.Complete();
+
     transforms.capacity = transforms.length + count;
-    for(int i = 0; i < count; ++i)
+    for (int i = 0; i < count; ++i)
     {
       float x = Random.Range(MinX, MaxX);
       float y = Random.Range(MinY, MaxY);
       GameObject obj = Instantiate(PrefabBoid, new Vector3(x, y, 0.0f), Quaternion.identity);
       transforms.Add(obj.transform);
     }
+    targetDirections = new NativeArray<Vector3>(transforms.length, Allocator.TempJob);
     BoidCount = transforms.length;
     UiJob.SetBoidCount(BoidCount);
   }
@@ -53,15 +72,20 @@ public class Manager : Patterns.Singleton<Manager>
   private void Update()
   {
     movementJobHandle.Complete();
+    randomMovementJobHandle.Complete();
+
     HandleInputs();
 
     movementJob = new MovementJob()
     {
-      speed = MaxSpeed,
-      deltaTime = Time.deltaTime
+      speed = 5.0f,
+      deltaTime = Time.deltaTime,
+      targetDirection = targetDirections,
+      rotationSpeed = 100.0f
     };
 
     movementJobHandle = movementJob.Schedule(transforms);
+
     JobHandle.ScheduleBatchedJobs();
   }
 
@@ -76,6 +100,25 @@ public class Manager : Patterns.Singleton<Manager>
     if(Input.GetKeyDown(KeyCode.Space))
     {
       AddBoids(BoidIncrementCount);
+    }
+  }
+
+  IEnumerator Coroutine_Random()
+  {
+    while (true)
+    {
+      if (useRandomRule)
+      {
+        randomMovementJob = new RandomMovementJob()
+        {
+          result = targetDirections,
+          weightRandom = 1.0f,
+          rnd = new Unity.Mathematics.Random((uint)mSeed.Next())
+        };
+
+        randomMovementJobHandle = randomMovementJob.Schedule(transforms);
+      }
+      yield return new WaitForSeconds(TickDurationRandom);
     }
   }
 }
--- Unity2D/Assets/Scripts/FlockingJob/MovementJob.cs
+++ Unity2D/Assets/Scripts/FlockingJob/MovementJob.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Collections;
+using Unity.Collections.LowLevel.Unsafe;
 using UnityEngine;
 using UnityEngine.Jobs;
 
@@ -7,11 +9,60 @@ public struct MovementJob : IJobParallelForTransform
 {
   public float speed;
   public float deltaTime;
+  [NativeDisableContainerSafetyRestriction]
+  public NativeArray<Vector3> targetDirection;
+  public float rotationSpeed;
 
   public void Execute(int index, TransformAccess transform)
   {
+    Vector3 dir = targetDirection[index];
+    dir.Normalize();
+
+    Vector3 rotatedVectorToTarget =
+      Quaternion.Euler(0, 0, 90) *
+      dir;
+
+    Quaternion targetRotation = Quaternion.LookRotation(
+      forward: Vector3.forward,
+      upwards: rotatedVectorToTarget);
+
+    transform.rotation = Quaternion.RotateTowards(
+      transform.rotation,
+      targetRotation,
+      rotationSpeed * deltaTime);
+
     Vector3 pos = transform.position;
     pos += speed * deltaTime * (transform.rotation * new Vector3(1.0f, 0.0f, 0.0f));
     transform.position = pos;
+  }
+}
+
+public struct RandomMovementJob : IJobParallelForTransform
+{
+  public NativeArray<Vector3> result;
+  public float weightRandom;
+  public Unity.Mathematics.Random rnd; 
+
+  public void Execute(int index, TransformAccess transform)
+  {      
+    float angle = transform.rotation.eulerAngles.z;
+
+    float rand = rnd.NextFloat(0.0f, 2.0f);
+    if (rand > 1.0f)
+    {
+      angle += 45.0f;
+    }
+    else
+    {
+      angle -= 45.0f;
+    }
+    //Debug.Log("Angle: " + angle.ToString("F2"));
+    //Debug.Log("Random: " + rand.ToString("F2"));
+    Vector3 dir = Vector3.zero;
+    dir.x = Mathf.Cos(Mathf.Deg2Rad* angle);
+    dir.y = Mathf.Sin(Mathf.Deg2Rad * angle);
+
+    result[index] = dir * weightRandom;
+    result[index].Normalize();
   }
 }
